39bbde91b976e559de539919d7ba2508be4df669
Prashant <meprashanttyagi2000@gmail.com>
2025-03-09 17:48:59 +0530

    added app.js

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+dotenv.config({
+    path: "./.env",
+});
+
+const app = express();
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json({limit: "20kb"}));
+app.use(express.urlencoded({extended: true, limit: "20kb"}));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+export { app }
