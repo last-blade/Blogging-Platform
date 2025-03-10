c8b1ff9a302a6b9dc048d4e6b976b26d612aa1bd
Prashant <meprashanttyagi2000@gmail.com>
2025-03-10 21:17:11 +0530

    fixed some bugs in login user controller

--- backend/src/controllers/userControllers/loginUser.controller.js
+++ backend/src/controllers/userControllers/loginUser.controller.js
@@ -1,5 +1,4 @@
-import { User } from "../../models/user.model.js";
-import { apiError, apiResponse, asyncHandler } from "../allImports.js";
+import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 
 const generateAccessAndRefreshToken = async (userId) => {
     const user = await User.findById(userId);
@@ -11,23 +10,21 @@ const generateAccessAndRefreshToken = async (userId) => {
 
     await user.save({validateBeforeSave: false})
 
-    return {accessToken, refreshToken, passwordCorrectOrNot}
-}
-
-const passwordCorrectOrNot = async () => {
-
+    return {accessToken, refreshToken}
 }
 
 const loginUser = asyncHandler(async (request, response) => {
     const {email, username, password} = request.body;
 
-    if ([!(email || username), !password].some((field) => field)) {
+    if (!(email || username) || !password) {
         throw new apiError(404, "Email or Username and Password are required.");
     };
 
     const foundUser = await User.findOne({
         $or: [{email}, {username}]
     });
+
+    console.log("Found user", foundUser)
 
     if(!foundUser){
         throw new apiError(400, "User with this email or username does not exists.")
@@ -41,7 +38,7 @@ const loginUser = asyncHandler(async (request, response) => {
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(foundUser?._id);
 
-    const loggedInUser = await User.findById(foundUser?._id).select("-password -refreshToken")
+    const loggedInUser = await User.findById(foundUser?._id).select("-password -refreshToken");
 
     const options = {
         httpOnly: true,
@@ -49,13 +46,11 @@ const loginUser = asyncHandler(async (request, response) => {
     }
 
     return response.status(200)
-    .cookies("Access Token", accessToken, options)
-    .cookies("Refresh Token", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
     .json(
-        new apiResponse(200, {user: loggedInUser, accessToken, refreshToken}, "Login successful")
+        new apiResponse(200, {user: loggedInUser}, "User loggedin successfully.")
     )
-
-
 });
 
 export {loginUser, generateAccessAndRefreshToken}
