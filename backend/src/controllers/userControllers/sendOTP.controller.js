9bfbeedc7adfbf56f2025b30ee26b3e8b90aad49
Prashant <meprashanttyagi2000@gmail.com>
2025-03-11 21:55:07 +0530

    added sendOTP controller

--- backend/src/controllers/userControllers/sendOTP.controller.js
+++ backend/src/controllers/userControllers/sendOTP.controller.js
@@ -1,13 +1,32 @@
 import { request } from "express";
-import { asyncHandler } from "../allImports.js";
+import { apiError, apiResponse, asyncHandler } from "../allImports.js";
 import { sendMail } from "../../utils/sendEmail.js";
+import { OTP } from "../../models/otp.model.js";
+
+const generateOTP = () => {
+    const OTP = Math.floor(1000 + Math.random() * 9000);
+    return OTP;
+}
 
 const sendOTP = asyncHandler(async (request, response) => {
     const {email} = request.body;
-    const OTP = Math.floor(1000 + Math.random() * 9000);
-    console.log(OTP);
 
-    sendMail(email, "Your Email OTP for registration on PixelPen", `Please find your One Time Password (OTP) ${OTP} and Please don't share this OTP with others.`)
+    const existingOTP = await OTP.findOne({email: email});
 
+    if(existingOTP){
+        throw new apiError(429, "Please wait before requesting a new OTP.")
+    }
+
+    const generatedOTP = generateOTP();
+
+    await OTP.findOneAndUpdate(
+        { email },
+        { generatedOTP, createdAt: new Date() },
+        { upsert: true, new: true }
+    );
+
+    await sendMail(email, "Your Email OTP for registration on PixelPen", `Please find your One Time Password (OTP) ${OTP} and Please don't share this OTP with others.`)
     
+    return response.status(200)
+    .json(200, {}, "OTP sent successfully.");
 })
