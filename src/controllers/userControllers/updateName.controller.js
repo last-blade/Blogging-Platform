e9ca6e20083126493b53ba4db2e8cc21caa7e7a6
no-prashant <prashant@mobiloitte.com>
2025-03-11 17:04:11 +0530

    add

--- src/controllers/userControllers/updateName.controller.js
+++ src/controllers/userControllers/updateName.controller.js
@@ -3,20 +3,30 @@ import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 const updateName = asyncHandler(async (request, response) => {
     const userId = request.user.id;
 
-    const foundUser = await User.findById(userId);
+
 
     const {fullname} = request.body;
 
     if(!fullname){
-        throw new apiError(404, "Fullname is required.")
+        throw new apiError(400, "Fullname is required.")
     }
 
-    foundUser.fullname = fullname;
+    const foundUser = await User.findByIdAndUpdate(userId, {
+        $set: {
+            fullname: fullname
+        }
+    }, {new: true});
 
-    foundUser.save({validateBeforeSave: false});
+    if(!foundUser){
+        throw new apiError(400, "User not found")
+    }
     
     return response.status(200)
     .json(new apiResponse(200, {}, "Fullname changed successfully"));
 });
 
 export {updateName}
+
+//    foundUser.fullname = fullname;
+
+// foundUser.save({validateBeforeSave: false});
