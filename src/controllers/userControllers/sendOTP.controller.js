b74a76af6ccf9d443b1232c048d259c7966dde2e
no-prashant <prashant@mobiloitte.com>
2025-03-11 18:08:23 +0530

    add

---
+++ src/controllers/userControllers/sendOTP.controller.js
@@ -0,0 +1,13 @@
+import { request } from "express";
+import { asyncHandler } from "../allImports.js";
+import { sendMail } from "../../utils/sendEmail.js";
+
+const sendOTP = asyncHandler(async (request, response) => {
+    const {email} = request.body;
+    const OTP = Math.floor(1000 + Math.random() * 9000);
+    console.log(OTP);
+
+    sendMail(email, "Your EMail OTP for registration on PixelPen", `Please find your One Time Password (OTP) ${OTP} and PLease don't share this OTP with others.`)
+
+    
+})
