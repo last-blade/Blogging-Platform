ab385ff146d13933b0e5207cd1890f442b851449
Prashant <meprashanttyagi2000@gmail.com>
2025-03-11 21:58:54 +0530

    added sendotp route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,12 +5,14 @@ import { authentication } from "../middlewares/authentication.middleware.js";
 import { logoutUser } from "../controllers/userControllers/logoutUser.controller.js";
 import { updateName } from "../controllers/userControllers/updateName.controller.js";
 import { updateEmail } from "../controllers/userControllers/updateEmail.controller.js";
+import { sendOTP } from "../controllers/userControllers/sendOTP.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(authentication, logoutUser);
+router.route("/send-otp").post(sendOTP);
 
 //Update controllers
 router.route("/update-fullname").patch(authentication, updateName);
