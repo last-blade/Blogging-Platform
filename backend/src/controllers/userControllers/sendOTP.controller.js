451e5ef69ed4ef5b02fdba206871f54982cbe933
no-prashant <prashant@mobiloitte.com>
2025-03-12 10:21:29 +0530

    fixed some bugs live saving the data in database

--- backend/src/controllers/userControllers/sendOTP.controller.js
+++ backend/src/controllers/userControllers/sendOTP.controller.js
@@ -4,8 +4,8 @@ import { sendMail } from "../../utils/sendEmail.js";
 import { OTP } from "../../models/otp.model.js";
 
 const generateOTP = () => {
-    const OTP = Math.floor(1000 + Math.random() * 9000);
-    return OTP;
+    const generatedOTP = Math.floor(1000 + Math.random() * 9000);
+    return generatedOTP;
 }
 
 const sendOTP = asyncHandler(async (request, response) => {
@@ -19,16 +19,20 @@ const sendOTP = asyncHandler(async (request, response) => {
 
     const generatedOTP = generateOTP();
 
+    await sendMail(email, "Your Email OTP for registration on PixelPen", `Please find your One Time Password (OTP) ${generatedOTP} and Please don't share this OTP with others.`)
+
     await OTP.findOneAndUpdate(
         { email },
-        { generatedOTP, createdAt: new Date() },
-        { upsert: true, new: true }
+        { $push: { otp: generatedOTP }, createdAt: new Date() },
+        { new: true, upsert: true } /* so 'upsert' is using because the otp im sending to an email does not exists in database, that's why I am using upsert
+        */
     );
-
-    await sendMail(email, "Your Email OTP for registration on PixelPen", `Please find your One Time Password (OTP) ${OTP} and Please don't share this OTP with others.`)
+    
     
     return response.status(200)
-    .json(200, {}, "OTP sent successfully.");
+    .json(
+        new apiResponse(200, {}, "OTP sent successfully.")
+    );
 });
 
 export {sendOTP}
