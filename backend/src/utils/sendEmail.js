25854856efbdbcf3e9884d3c5935fecc89154007
no-prashant <prashant@mobiloitte.com>
2025-03-12 10:21:56 +0530

    fiexd some bugs in sendemail utils

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,22 +1,28 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
     service: "gmail", 
     auth: {
-        user: `${process.env.SENDERS_EMAIL}`, // yahan par apni email jis se mail send karna hai user ko
-        pass: `${process.env.EMAIL_APP_PASSWORD}`, // yahan par app password likhna hai
+        user: `${process.env.SENDERS_EMAIL}`, 
+        pass: `${process.env.EMAIL_APP_PASSWORD}` 
     },
 });
 
 const sendMail = async (to, subject, text) => {
     try {
+        console.log("To", to);
+        console.log("subject", subject)
+        console.log("text", text)
         const info = await transporter.sendMail({
-            from: `"${process.env.SENDERS_NAME}" <${process.env.SENDERS_EMAIL}>`, // Sender kaa email address
-            to, // Recipient kaa email address
-            subject, // subject of email
-            text, // email kaa content or body hai yeh
+            from: `"${process.env.SENDERS_NAME}" <${process.env.SENDERS_EMAIL}>`,
+            to, 
+            subject,
+            text,
         });
         console.log("Email sent: ", info.messageId);
+        return;
     } catch (error) {
         console.error("Error sending email: ", error);
     }
