4b6b36fa2573118b72a7acbbc53d9df5336dd87e
Prashant <meprashanttyagi2000@gmail.com>
2025-03-14 13:26:54 +0530

    adde like-comment controller

---
+++ backend/src/controllers/userControllers/likeComment.controller.js
@@ -0,0 +1,27 @@
+import { apiResponse, asyncHandler } from "../allImports.js";
+
+const likeComment = asyncHandler(async (request, response) => {
+    const {commentId} = request.params;
+
+    if(!commentId){
+        throw new apiError(404, "Comment does not exists or something went wrong")
+    }
+
+    const userId = request.user?.id;
+
+    if(!userId){
+        throw new apiError(400, "Please login!")
+    }
+
+    await Like.create({
+        likedComment: commentId,
+        likedBy: userId,
+    });
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Comment Liked.")
+    )
+});
+
+export {likeComment}
