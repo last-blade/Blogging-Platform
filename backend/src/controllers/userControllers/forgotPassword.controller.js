ad683045ed3488831223615669be032a88243606
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 21:13:37 +0530

    added email body message aftrer resetting the password

--- backend/src/controllers/userControllers/forgotPassword.controller.js
+++ backend/src/controllers/userControllers/forgotPassword.controller.js
@@ -1,3 +1,4 @@
+import { sendMail } from "../../utils/sendEmail.js";
 import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 
 const forgotPassword = asyncHandler(async (request, response) => {
@@ -12,15 +13,28 @@ const forgotPassword = asyncHandler(async (request, response) => {
         throw new apiError(400, "Both passwords do not match!")
     }
 
-    const foundUser = await User.findOneAndUpdate({email: incomingEmail}, {
-        $set: {
-            password: newPassword
-        }
-    });
+    const foundUser = await User.findOneAndUpdate({email: incomingEmail});
+
+    foundUser.password = newPassword;
+    foundUser.save({validateBeforeSave: false})
 
     if(!foundUser){
         throw new apiError(404, "Session expired!")
     }
+
+    const bodyOfEmail = `
+    <div style="font-family: Arial, sans-serif; padding: 20px; line-height: 1.5;">
+        <h2 style="color: #333333;">Your PixelPen Account Password Has Been Successfully Changed!</h2>
+        <p>Hey ${foundUser.fullname},</p>
+        <p>We wanted to let you know that your password has been successfully updated. If you made this change, you're all set!</p>
+        <p>If you didn't change your password, please reset your password immediately or contact our support team.</p>
+        <br />
+        <p>Thank you for using PixelPen!</p>
+        <p>Team PixelPen</p>
+    </div>`;
+
+
+    await sendMail(incomingEmail, "PixelPen account password changed", bodyOfEmail)
 
     return response.status(200)
     .json(
