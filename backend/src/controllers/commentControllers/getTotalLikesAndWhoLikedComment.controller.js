7033fb1fbdc911766407ecc7bcc84aed549c1844
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 14:16:06 +0530

    added getTotalLikesAndWhoLikedComment

--- backend/src/controllers/commentControllers/getTotalLikesAndWhoLikedComment.controller.js
+++ backend/src/controllers/commentControllers/getTotalLikesAndWhoLikedComment.controller.js
@@ -1,7 +1,43 @@
+import { Comment } from "../../models/comment.model.js";
 import { asyncHandler } from "../allImports.js";
 
 const getTotalLikesAndWhoLikedComment = asyncHandler(async (request, response) => {
     
+    const {blogId} = request.params;
+    
+    const commentsWithLikes = await Comment.aggregate([
+        {
+            $match: {
+                commentedBlog: blogId
+            }
+        },
+
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "likedComment",
+                as: "allLikesOnComment"
+            }
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "allLikesOnComment.likedBy",
+                foreignField: "_id",
+                as: "whoLiked"
+            }
+        },
+
+        {
+            $addFields: {
+                totalLikesOnComment: {
+                    $size: "$allLikesOnComment",
+                }
+            }
+        }
+    ]);
 });
 
 export {getTotalLikesAndWhoLikedComment}
