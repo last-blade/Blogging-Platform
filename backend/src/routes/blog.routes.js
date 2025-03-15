346c32f1b3ee4ca4d687204b67a1c0efa7ee7758
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 14:38:07 +0530

    added blog routes

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -4,12 +4,16 @@ import { createBlog } from "../controllers/blogControllers/createBlog.controller
 import { fetchBlog } from "../controllers/blogControllers/fetchBlog.controllers.js";
 import { fetchAllUsersBlogs } from "../controllers/blogControllers/fetchAllUsersBlogs.controller.js";
 import { editBlog } from "../controllers/blogControllers/editBlog.controller.js";
+import { likeBlog } from "../controllers/blogControllers/likeBlog.controller.js";
+import { getTotalLikesAndWhoLikedBlog } from "../controllers/blogControllers/getTotalLikesAndWhoLikedBlog.controller.js";
 
 const router = Router();
 
 router.route("/create-blog").post(authentication, createBlog);
 router.route("/fetch-blog").get(authentication, fetchBlog);
 router.route("/fetch-all-blogs").get(fetchAllUsersBlogs);
+router.route("/like-blog").post(authentication, likeBlog);
+router.route("/get-total-likes-and-who-liked-blog").get(getTotalLikesAndWhoLikedBlog);
 
 //update routes
 router.route("/edit-blog").patch(authentication, editBlog);
