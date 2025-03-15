2a78f69497c59f419b7be19732319e5ebf3b9221
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 20:49:00 +0530

    added validation for incoming otp

--- backend/src/controllers/userControllers/verifyForgotPasswordOTP .controller.js
+++ backend/src/controllers/userControllers/verifyForgotPasswordOTP .controller.js
@@ -5,6 +5,10 @@ const verifyForgotPasswordOTP  = asyncHandler(async (request, response) => {
     const incomingEmail = request.cookies.Email;
     const {incomingOTP} = request.body;
 
+    if(!incomingOTP){
+        throw new apiError(404, "OTP required!")
+    }
+
     if(!incomingEmail){
         throw new apiError(400, "OTP expired, resend OTP")
     }
