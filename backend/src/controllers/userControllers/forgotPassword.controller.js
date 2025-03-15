c8124f8161ff02d6db87424977a8ac729f6abec5
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 20:58:28 +0530

    fixed error while fetching the email from the cookies

--- backend/src/controllers/userControllers/forgotPassword.controller.js
+++ backend/src/controllers/userControllers/forgotPassword.controller.js
@@ -2,7 +2,7 @@ import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 
 const forgotPassword = asyncHandler(async (request, response) => {
     const {newPassword, confirmNewPassword} = request.body;
-    const {email} = request.cookies?.Email;
+    const incomingEmail = request.cookies?.Email;
     
     if([!newPassword, !confirmNewPassword].some((inputField) => inputField === "")){
         throw new apiError(400, "All fields are required.")
@@ -12,14 +12,14 @@ const forgotPassword = asyncHandler(async (request, response) => {
         throw new apiError(400, "Both passwords do not match!")
     }
 
-    const foundUser = await User.findOneAndUpdate({email}, {
+    const foundUser = await User.findOneAndUpdate({email: incomingEmail}, {
         $set: {
             password: newPassword
         }
     });
 
     if(!foundUser){
-        throw new apiError(404, "User not found!")
+        throw new apiError(404, "Session expired!")
     }
 
     return response.status(200)
