69d420eaa42cabf6ac41e830fb5ff22a15625f2e
Prashant <meprashanttyagi2000@gmail.com>
2025-03-15 20:26:40 +0530

    added verofy otp in utils

---
+++ backend/src/utils/verifyOtpHelper.js
@@ -0,0 +1,23 @@
+import { OTP } from "../models/otp.model.js";
+
+
+const verifyOtpHelper = async (incomingEmail, incomingOTP) => {
+
+    const foundUser = await OTP.findOne({incomingEmail});
+
+    if(!foundUser){
+        throw new apiError(400, "OTP expired, send OTP again.")
+    }
+
+    const databaseOTP = foundUser.otp[0];
+
+    if(incomingOTP !== databaseOTP){
+        throw new apiError(400, "Incorrect OTP.")
+    }
+
+    await OTP.findOneAndDelete({ incomingEmail });
+
+    return true;
+}
+
+export {verifyOtpHelper}
