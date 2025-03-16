6faea6736c18132f28de793f9ed2fc26d495c129
Prashant <meprashanttyagi2000@gmail.com>
2025-03-16 17:40:50 +0530

    removed console logs

--- backend/src/controllers/blogControllers/createBlog.controller.js
+++ backend/src/controllers/blogControllers/createBlog.controller.js
@@ -4,7 +4,6 @@ import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 const createBlog = asyncHandler(async (request, response) => {
     try {
         const {blogName, blogContent} = request.body;
-        // console.log("Blogname", blogName);
 
         const createdBlog = await Blog.create({
             blogName,
@@ -18,9 +17,6 @@ const createBlog = asyncHandler(async (request, response) => {
             }
         }, {new: true}).select("-password -refreshToken")
 
-        // updatedUser.save({validateBeforeSave: false})
-
-        console.log("createdBlog", createdBlog)
 
         return response.status(200)
         .json(new apiResponse(200, createdBlog, "Blog Created successfully."))
