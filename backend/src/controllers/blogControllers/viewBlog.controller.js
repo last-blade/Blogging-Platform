b74d3083a82f58a2abd188753d2185f17dbf622f
Prashant <meprashanttyagi2000@gmail.com>
2025-03-16 17:28:23 +0530

    added view-blog controller

---
+++ backend/src/controllers/blogControllers/viewBlog.controller.js
@@ -0,0 +1,29 @@
+import { Blog } from "../../models/blog.model.js";
+import { apiError, apiResponse, asyncHandler } from "../allImports.js";
+
+const viewBlog = asyncHandler(async (request, response) => {
+    const {blogId} = request.params;
+
+    if(!blogId){
+        throw new apiError(404, "Blog not found or reload the page.")
+    }
+
+    // const userId = request?.user.id;
+
+    // if(!userId){
+    //     throw new apiError(400, "Please login!")
+    // }
+
+    const foundBlog = await Blog.findById(blogId).populate("blogOwner", "fullname email avatar");
+
+    if(!foundBlog){
+        throw new apiError(404, "Blog does not exists!")
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, foundBlog, "Blog fetched successfully.")
+    )
+});
+
+export {viewBlog}
