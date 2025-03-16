4ff8cdd9b8ab27039d7c76a92b74cc19e3862e40
Prashant <meprashanttyagi2000@gmail.com>
2025-03-16 16:57:20 +0530

    added delete-blog route

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -6,16 +6,20 @@ import { fetchAllUsersBlogs } from "../controllers/blogControllers/fetchAllUsers
 import { editBlog } from "../controllers/blogControllers/editBlog.controller.js";
 import { likeBlog } from "../controllers/blogControllers/likeBlog.controller.js";
 import { getTotalLikesAndWhoLikedBlog } from "../controllers/blogControllers/getTotalLikesAndWhoLikedBlog.controller.js";
+import { deleteBlog } from "../controllers/blogControllers/deleteBlog.controller.js";
 
 const router = Router();
 
 router.route("/create-blog").post(authentication, createBlog);
-router.route("/fetch-blog").get(authentication, fetchBlog);
+router.route("/fetch-user-blogs").get(authentication, fetchBlog);
 router.route("/fetch-all-blogs").get(fetchAllUsersBlogs);
-router.route("/like-blog").post(authentication, likeBlog);
+router.route("/like-blog/:blogId").post(authentication, likeBlog);
 router.route("/get-total-likes-and-who-liked-blog").get(getTotalLikesAndWhoLikedBlog);
 
-//update routes
-router.route("/edit-blog").patch(authentication, editBlog);
+//Update Routes
+router.route("/edit-blog/:blogId").patch(authentication, editBlog);
+
+//Delete Routes
+router.route("/delete-blog/:blogId").delete(authentication, deleteBlog)
 
 export default router;
