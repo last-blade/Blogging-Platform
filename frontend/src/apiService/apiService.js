c93ef3d77283a9ee13826807bc963935972eafc8
Prashant <meprashanttyagi2000@gmail.com>
2025-03-19 20:33:52 +0530

    added api service

---
+++ frontend/src/apiService/apiService.js
@@ -0,0 +1,6 @@
+import * as authApi from "../api/authApi.js";
+import * as blogApi from "../api/blogApi";
+import * as commentApi from "../api/commentApi";
+import * as otpApi from "../api/otpApi";
+
+export { authApi, blogApi, commentApi, otpApi };
