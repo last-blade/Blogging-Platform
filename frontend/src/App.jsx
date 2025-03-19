18fe3f62e1a4cb1dd3485ddd64c7d7b81e54129b
Prashant <meprashanttyagi2000@gmail.com>
2025-03-19 21:09:14 +0530

    tested the api of fetch all blogs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { blogApi } from './apiService/apiService'
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const res = await blogApi.fetchAllBlogs();
+        console.log("data", res.data)
+        setBlogs(res.data.data);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+      }
+    }
+    fetchBlogs();
+  }, [])
+
   return (
-    <div className='text-red-600 pt-3'>App</div>
+    <div className='text-red-600 pt-3'>
+      <h1>All Blogs</h1>
+      {blogs.map((blog) => (
+        <div key={blog.id}>{blog.blogName}</div>
+      ))}
+    </div>
   )
 }
 
