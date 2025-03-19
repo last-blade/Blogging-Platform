8864055319f3bc8aafa61af6505c43009837e34c
Prashant <meprashanttyagi2000@gmail.com>
2025-03-19 21:10:06 +0530

    updated to correct api routes

--- frontend/src/api/blogApi.js
+++ frontend/src/api/blogApi.js
@@ -1,25 +1,25 @@
 import API from "./axiosInstance";
 
 //Create Blog
-export const createBlog = async (blogData) => API.post("/create-blog", blogData);
+export const createBlog = async (blogData) => API.post("blog/create-blog", blogData);
 
 //Fetch User's Blogs
-export const fetchUserBlogs = async () => API.get("/fetch-user-blogs");
+export const fetchUserBlogs = async () => API.get("blog/fetch-user-blogs");
 
 //Fetch All Blogs
-export const fetchAllBlogs = async () => API.get("/fetch-all-blogs");
+export const fetchAllBlogs = async () => API.get("blog/fetch-all-blogs");
 
 //View Single Blog
-export const viewBlog = async (blogId) => API.get(`/view-blog/${blogId}`);
+export const viewBlog = async (blogId) => API.get(`blog/view-blog/${blogId}`);
 
 //Like Blog
-export const likeBlog = async (blogId) => API.post(`/like-blog/${blogId}`);
+export const likeBlog = async (blogId) => API.post(`blog/like-blog/${blogId}`);
 
 //Get Blog Likes & Who Liked
-export const getBlogLikes = async (blogId) => API.get(`/get-total-likes-and-who-liked-blog/${blogId}`);
+export const getBlogLikes = async (blogId) => API.get(`blog/get-total-likes-and-who-liked-blog/${blogId}`);
 
 //Edit Blog
-export const editBlog = async (blogId, updatedData) => API.patch(`/edit-blog/${blogId}`, updatedData);
+export const editBlog = async (blogId, updatedData) => API.patch(`blog/edit-blog/${blogId}`, updatedData);
 
 //Delete Blog
-export const deleteBlog = async (blogId) => API.delete(`/delete-blog/${blogId}`);
+export const deleteBlog = async (blogId) => API.delete(`blog/delete-blog/${blogId}`);
