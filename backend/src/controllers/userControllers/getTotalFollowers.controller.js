0c7615e16f0848cc55f20e966568161387bc2a9e
last-blade <meprashanttyagi2000@gmaIL.com>
2025-03-25 14:51:46 +0530

    added getTotalfollowers-controller

--- backend/src/controllers/userControllers/getTotalFollowers.controller.js
+++ backend/src/controllers/userControllers/getTotalFollowers.controller.js
@@ -10,16 +10,53 @@ const getTotalFollwers = asyncHandler(async (request, response) => {
         throw new apiError(404, "User not found, login again.")
     }
 
-    const totalFollowers = await Follow.aggregate([
+    const allTotalFollowers = await Follow.aggregate([
         
         //first-pipeline
         {
             $match: {
                 followedTo: userId,
             }
+        },
+        //second-pipeline
+
+        {
+            $group: {
+                _id: "$followedTo",
+                totalFollowers: {$sum: 1},
+                followerIds: {$push: "$followedBy"}
+            }
+        },
+
+        //third-pipeline
+        {
+            $lookup: {
+                from: "users",
+                localField: "followerIds",
+                foreignField: "_id",
+                as: "followerDetails"
+            }
+        },
+
+        //fourth-pipeline
+        {
+            $project: {
+                _id: 0,
+                totalFollowers: 1,
+                followerDetails: {
+                    _id: 1,
+                    fullname: 1,
+                    username: 1,
+                    email: 1
+                }
+            }
         }
     ]);
-    
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, allTotalFollowers, "Total followers fetched successfully.")
+    );
 });
 
 export {getTotalFollwers}
