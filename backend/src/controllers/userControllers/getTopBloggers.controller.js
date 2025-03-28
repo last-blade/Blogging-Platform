df54094c4250d4710d0759af23d10993511b3542
last-blade <meprashanttyagi2000@gmaIL.com>
2025-03-28 16:16:18 +0530

    writing || in progress

--- backend/src/controllers/userControllers/getTopBloggers.controller.js
+++ backend/src/controllers/userControllers/getTopBloggers.controller.js
@@ -1,7 +1,18 @@
-import { asyncHandler } from "../allImports.js";
+import { fetchAllUsers } from "../../utils/fetchAllUsers.js";
+import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
 
 const getTopBloggers = asyncHandler(async (request, response) => {
-    
+    const allUsers = await fetchAllUsers();
+
+    if(!allUsers){
+        throw new apiError(404, "No user found!")
+    }
+
+    console.log("all users:\n", allUsers);
+    return response.status(200)
+    .json(
+        new apiResponse(200, allUsers, "all users")
+    )
 });
 
 export {getTopBloggers}
