249c405f53f07a7248ce2a9e1c75d872291a8c34
last-blade <meprashanttyagi2000@gmaIL.com>
2025-03-29 15:25:12 +0530

    fixed api route

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,27 +1,27 @@
 import API from "./axiosInstance";
 
 //Register User
-export const registerUser = async (userData) => API.post("/register", userData);
+export const registerUser = async (userData) => API.post("user/register", userData);
 
 //Login User
-export const loginUser = async (credentials) => API.post("/login", credentials);
+export const loginUser = async (credentials) => API.post("/user/login", credentials);
 
 //Logout User
-export const logoutUser = async () => API.post("/logout");
+export const logoutUser = async () => API.post("/user/logout");
 
 //Update Full Name
-export const updateFullName = async (nameData) => API.patch("/update-fullname", nameData);
+export const updateFullName = async (nameData) => API.patch("/user/update-fullname", nameData);
 
 //Update Email
-export const updateEmail = async (emailData) => API.patch("/update-email", emailData);
+export const updateEmail = async (emailData) => API.patch("/user/update-email", emailData);
 
 //Update Username
-export const updateUsername = async (usernameData) => API.patch("/update-username", usernameData);
+export const updateUsername = async (usernameData) => API.patch("/user/update-username", usernameData);
 
 //Change Password
-export const changePassword = async (passwordData) => API.patch("/change-password", passwordData);
+export const changePassword = async (passwordData) => API.patch("/user/change-password", passwordData);
 
 //Forgot Password Process
-export const sendForgotPasswordOTP = async (email) => API.post("/send-forgot-password-otp", email);
-export const verifyForgotPasswordOTP = async (otpData) => API.post("/verify-forgot-password-otp", otpData);
-export const resetPassword = async (passwordData) => API.patch("/forgot-password", passwordData);
+export const sendForgotPasswordOTP = async (email) => API.post("/user/send-forgot-password-otp", email);
+export const verifyForgotPasswordOTP = async (otpData) => API.post("/user/verify-forgot-password-otp", otpData);
+export const resetPassword = async (passwordData) => API.patch("/user/forgot-password", passwordData);
