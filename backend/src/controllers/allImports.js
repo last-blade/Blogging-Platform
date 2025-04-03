591d493c4386313222db39b751904a1e08df7af8
last-blade <meprashanttyagi2000@gmaIL.com>
2025-04-03 15:22:36 +0530

    added new import

--- backend/src/controllers/allImports.js
+++ backend/src/controllers/allImports.js
@@ -3,4 +3,5 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import { Follow } from "../models/follow.model.js";
-export {asyncHandler, apiError, apiResponse, User, Follow}
+import { View } from "../models/view.model.js";
+export {asyncHandler, apiError, apiResponse, User, Follow, View}
