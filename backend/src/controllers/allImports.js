c4a7164c29c0100325f5358945ff87d4111696ef
last-blade <meprashanttyagi2000@gmaIL.com>
2025-04-03 14:01:09 +0530

    added follow model in all imports file to acess easily

--- backend/src/controllers/allImports.js
+++ backend/src/controllers/allImports.js
@@ -2,4 +2,5 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
-export {asyncHandler, apiError, apiResponse, User}
+import { Follow } from "../models/follow.model.js";
+export {asyncHandler, apiError, apiResponse, User, Follow}
